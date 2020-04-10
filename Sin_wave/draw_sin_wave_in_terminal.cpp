f1e955bad343b464a4de802957c8f45ae572df58
SubhoBasak <subhobasak50@gmail.com>
2020-04-10 13:16:16 +0530

    added animation

--- Sin_wave/draw_sin_wave_in_terminal.cpp
+++ Sin_wave/draw_sin_wave_in_terminal.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <unistd.h>
 
 using namespace std;
 
-int main(){
+void show_frame(int start){
+	system("clear");
 	int height = 20, width = 180, std = 20;
 	int half_h = int(height/2);
 	short int mat[height][width] = {0};
 	int i, j;
 	
 	for(i = 0; i < width; i++){
-		j = int(sin((width/std)*i*3.14/180)*half_h);
+		j = int(sin((width/std)*(i+start)*3.14/180)*half_h);
 		if(j > 0)
 			mat[half_h-j][i] = 1;
 		else if(j == 0)
@@ -30,6 +32,13 @@ int main(){
 			}
 		}
 		cout << endl;
+	}
+}
+
+int main(){
+	for(int i = 0; i < 200; i++){
+		show_frame(i);
+		usleep(50000);
 	}
 	
 	return 0;
